dcaa9929062453d249e224acbf40b7f119e34e7c
Jimmy Li <contact@jimmyzmli.ca>
2012-07-23 07:42:45 -0400

    Finished layout for landing page

--- index.php
+++ index.php
@@ -16,3 +16,151 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 ?>
+<?php
+
+$posts = get_posts();
+
+?>
+<?php get_header(); ?>
+    <section id="body">
+          <section id="main">
+	    <section id="top-promo">
+	      <h2>Top News Story</h2>
+	      <section class="promo-title">
+		<h2>News Title</h2>
+		<article class="promo-desc">
+		  NEWS NEWS NEWS NEWS NEWS LOL
+		</article>
+	      </section>
+	      <img src="noimg" class="promo-img"/>
+	      <div style="clear:both"></div>
+	    </section>
+
+	    <section class="news-promo">
+	      <h2>World News</h2>
+	      <section class="news-tile-half">
+		<section class="promo-story">
+		  <img src="noimg" class="promo-img"/>
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>
+		</section>
+	      </section>
+	      <section class="news-tile-quart">
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>	      
+	      </section>
+	      <section class="news-tile-quart">
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>	      
+	      </section>
+	      <div style="clear:both"></div>
+	    </section>
+
+	    <section class="news-promo">
+	      <h2>Business</h2>
+	      <section class="news-tile-half">
+		<section class="promo-story">
+		  <img src="noimg" class="promo-img"/>
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>
+		</section>
+	      </section>
+	      <section class="news-tile-half">
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>	      
+	      </section>
+	      <div style="clear:both"></div>
+	    </section>
+
+	    <section class="news-promo">
+	      <h2>Sports</h2>
+	      <section class="news-tile-half">
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>	      
+	      </section>
+	      <div style="clear:both"></div>
+	    </section>
+
+	    <section class="news-promo">
+	      <h2>Entertainment</h2>
+	      <section class="news-tile-half">
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>
+		<section class="promo-story">
+		  <section class="promo-title">Iraq Attack etc.</section>
+		  <section class="promo-desc">Iraq attack</section>		
+		</section>
+		<div style="clear:both"></div>
+	      </section>
+	    </section>
+	    
+	    <div style="clear:both"></div>
+	  </section>
+
+	  <section id="side">
+	    
+	    <section id="about">
+	      <h2>About News Letter</h2>
+	      <div class="desc">About here BLAH BLAH BLAH BLAH BLAH</div>
+	    </section>
+	    
+	    <section id="top-spotlight">
+	      <h2>Featured</h2>
+	      
+	      <article class="promo-story">
+		<img src="noimg" class="promo-img"/>
+		<section class="promo-title">
+		  Holy shit! Birds poop.
+		</section>
+		<section class="promo-desc">
+		  TIL Birds can poop. Blah blah blah lorem ipsum.
+		</section>
+		<div style="clear:both"></div>
+	      </article>
+
+	      <article class="promo-story">
+		<img src="noimg" class="promo-img"/>
+		<section class="promo-title">
+		  Holy shit! Birds poop.
+		</section>
+		<section class="promo-desc">
+		  TIL Birds can poop. Blah blah blah lorem ipsum.
+		</section>
+		<div style="clear:both"></div>
+	      </article>
+	      
+	    </section>
+
+	    <section class="updates">
+	      <h2>Updates</h2>
+	      <article class="promo-story">
+		<section class="promo-author">JzL</section>
+		<section class="promo-date">Mon Jul 23</section>		
+		<section class="promo-desc">TWITT TWITT. PFFTOOOOT.</section>
+	      </article>
+	      <article class="promo-story">
+		<section class="promo-author">JzL</section>
+		<section class="promo-date">Mon Jul 23</section>		
+		<section class="promo-desc">TWITT TWITT. PFFTOOOOT.</section>
+	      </article>
+	    </section>
+
+	    <section class="survey-bar">
+	      <h2>Surveys</h2>
+	      ABCDEFG
+	    </section>
+	    
+	  </section>
+	  <div style="clear:both"></div>
+    </section>
+<?php get_footer() ?>
