0337b27eca5c3147a4e5ad2b8482631aaff258bc
Jimmy Li <contact@jimmyzmli.ca>
2012-07-30 06:09:12 -0400

    Added dynamic headers

---
+++ header.js
@@ -0,0 +1,35 @@
+jQuery(function($) {
+    var g = window;
+    var hiddens = $("#nav-bar1 .hidden-nav-section");
+    var clrs = ['#F30C23', '#FFBC00', 'green', 'red', 'blue','yellow','lightblue','green'];
+    
+    g.i = 0;
+    
+    $("#nav-bar1-expand").css('visibility','hidden').append(
+	hiddens.detach()
+    ).children("[style]").removeAttr("style");
+  
+    hiddens.each(function() {
+	$(this).css('position','relative')
+	    .prepend( $("<div>")
+		      .css('background-color', clrs[g.i++])
+		      .width(5).height($(this).parent().height())
+		      .css('position','absolute').css('top','0').css('left','0') );
+    });
+
+    $("#nav-bar1 ul>li:not(#nav-expand-btn) a").each( function() {
+	$(this)
+	    .append( $("<div>")
+			.css('background-color', clrs[g.i++])
+			.width( $(this).parent().width() ).height(3)
+			.css('visibility','hidden')
+		        .addClass('highlighter') )
+	    .bind('mouseover', function(){ $('>.highlighter',this).css('visibility','visible'); } )
+	    .bind('mouseout', function(){ $('>.highlighter',this).css('visibility','hidden'); } );
+	
+    });
+
+    $("#nav-bar1-expand").removeAttr('style').css('display','none');
+    
+    $("#nav-expand-btn").bind( 'click', function() { $("#nav-bar1-expand").slideToggle(); } );
+});
--- header.php
+++ header.php
@@ -17,30 +17,94 @@
 */
 
 $prefix = get_template_directory_uri();
-
-function nws_get_title() {
-  if( is_single() ) {
-    single_post_title();
-  }else if( is_home() || is_front_page() ) {
-    bloginfo('name');
-    print ' | ';
-    bloginfo('description');
-    get_page_number();
-  }else if( is_search() ) {
-    printf( 'Search Results for %s - ', wp_specialchars($s) );
-    get_page_number();
-  }else if( is_404() ) {
-    bloginfo('name');
-    print ' | 404 - Not found';
-  }else {
-    bloginfo('name');
-    wp_title('|');
-    get_page_number();
-  }
+$barname = null;
+if( is_single() ) {
+  $title = single_post_title();
+  $barname = "single";
+}else if( is_home() || is_front_page() ) {
+  $title = get_bloginfo('name') . '|' . get_bloginfo('description');
+  $barname = "landing";
+}else if( is_search() ) {
+  $title = sprintf( 'Search Results for %s - %d ', wp_specialchars($s), get_page_number() );
+  $barname = "search";
+}else if( is_404() ) {
+  $title = get_bloginfo('name') . ' | 404 - Not found';
+}else {
+  $title = get_bloginfo('name') . wp_title('|') . get_page_number();
 }
+/* Exported names */
+$GLOBALS["barname"] = $barname;
 
 if( is_singular() ) {
   wp_enqueue_script( 'comment-reply' );
+}
+
+class TopMenuWalker extends Walker_Nav_Menu {
+  private $i = 0, $limit = 1;
+  public function __construct( $n ) {
+    $this->limit = $n-1;
+  }
+  function start_el( &$out, $item, $depth ) {
+    if( $depth == 0 ) $this->i++;
+    if( $depth == 0 && $this->i == $this->limit+1 )
+      $out .= '<li id="nav-expand-btn"><a href="#">More</a></li>';    
+    $out .= sprintf( '<li%3$s><a href="%2$s">%1$s</a>',
+		     esc_attr($item->title),
+		     esc_attr($item->url),
+		     ($this->i > $this->limit && $depth == 0) ?
+		     ' style="display:none" class="hidden-nav-section" ' : '');
+  }
+
+  function start_lvl( &$out, $depth = 0 ) {
+    $out .= sprintf('<ul%s>', ($depth >= 0) ? ' class="nav-dropmenu" ' : '') ;
+  }
+
+  function end_lvl( &$out, $depth = 0 ) {
+    $out .= '<div style="clear:both"></div>'.'</ul>';
+    //$out .= '</ul>';
+  }
+}
+
+$navmenu_opts = array(
+		      'container_id'=>'nav-bar1',
+		      'container_class'=>'nav-bar-horizontal',
+		      'menu_class'=>'clearfix'
+		      );
+
+
+function output_cat_nav_menu() {
+  global $navmenu_opts;
+  $out = "";
+  $k = new TopMenuWalker(4);
+  $header_cat = get_all_category_ids();
+  
+  $k->start_lvl( $out, -1 );
+  foreach( $header_cat as $i=>$cat_ID ) {
+    $item = new stdClass;
+    $c =  get_category( $cat_ID );
+    $item->url = get_category_link($cat_ID);
+    $item->title = $c->name;
+    $k->start_el( $out, $item, 0 );
+    $k->end_el( $out, $item, 0 );    
+  }
+  $k->end_lvl( $out, -1 );
+  print '<div class="nav-bar-horizontal" id="nav-bar1"><ul class="clearfix">'.$out.'</ul></div>';
+}
+
+function output_page_nav_menu() {
+  $out = "";
+  $k = new TopMenuWalker(10000);
+  $pagelist = get_pages();
+  $k->start_lvl( $out, -1 );
+  foreach( $pagelist as $i=>$p ) {
+    $item = new stdClass;
+    $item->url = get_permalink( $p->ID );
+    $item->title = $p->post_title;
+    $k->start_el( $out, $item, 0 );
+    $k->end_el( $out, $item, 0 );    
+  }
+  $k->end_lvl( $out, -1 );
+  print '<div class="nav-bar-horizontal" id="nav-bar2">'.$out.'</div>';
 }
 
 ?>
@@ -48,13 +112,16 @@ if( is_singular() ) {
 <html <?php language_attributes() ?>>
   <head>
     <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>" charset="<?php bloginfo('charset') ?>"/>
-    <title><?php nws_get_title(); ?></title>
+    <title><?=$title?></title>
     <link rel="alternative" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="News"/>
     <link rel="alternative" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="Comments"/>
     <link rel="pingback" href="<?php bloginfo('pingback_url') ?>"/>
     <!-- Theme Style Loading -->
     <link href="<?php bloginfo('stylesheet_url') ?>" rel="stylesheet"/> <!-- Main Stylesheet -->
     <?php if( is_single() ) : ?><link rel="stylesheet" href="<?=$prefix?>/single.css"/><?php endif; ?>
+    <!-- Script Loading -->
+    <script type="text/javascript" src="<?=$prefix?>/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?=$prefix?>/header.js"></script>
     <?php wp_head(); ?>
   </head>
   <body id="page">
@@ -64,29 +131,21 @@ if( is_singular() ) {
       <div class="seg3"></div>
     </div>
     <header>
-      <div id="top-banner">
+      <a id="top-banner" href="<?=site_url()?>">
 	<img src="http://placehold.it/960x125"/>
-      </div>
+      </a>
       <nav id="top-nav">
-	<div id="branding">
-	  <?php bloginfo('name') ?>
-	</div>
-	<ul class="nav-bar-horizontal" id="nav-bar1">
-	  <li>News</li>
-	  <li>News</li>
-	  <li>News</li>
-	  <li>More...</li>
-	</ul>
+	<a id="branding" href="<?=site_url()?>">
+	  <img alt="<?=get_bloginfo('name')?>" src="http://placehold.it/200x32"/>
+	</a>
+  <?php wp_nav_menu( array_merge( array("theme_location"=>"primary_menu",'walker'=>new TopMenuWalker(4),"fallback_cb"=>'output_cat_nav_menu'), $navmenu_opts) ); ?>
 	<div id="search-box">
-	  <input type="text"/>
+	  <?php get_search_form(); ?>
 	</div>
 	<div style="clear:both"></div>
       </nav>
-      <nav class="nav-bar-horizontal" id="nav-bar2">
-	<li>Home</li>
-	<li>About</li>
-	<div style="clear:both"></div>
-      </nav>	
+      <nav class="nav-bar-horizontal clearfix" id="nav-bar1-expand"></nav>
+  <?php wp_nav_menu( array_merge( array("theme_location"=>"secondary_menu",'walker'=>new TopMenuWalker(1000),"fallback_cb"=>'output_page_nav_menu'), $navmenu_opts) ); ?>
       <div class="info-bar">
 	Hey I just met you, and this is crazy... but the server's down, for maintance baby
       </div>
